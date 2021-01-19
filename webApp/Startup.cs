ecd25cc8699fa957de1d734109b0e54df443263b
jacobzouzou <jacob.zouzou@gmail.com>
2021-01-19 12:25:42 +0100

    commit testing

--- webApp/Startup.cs
+++ webApp/Startup.cs
@@ -36,6 +36,7 @@ namespace webApp
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
